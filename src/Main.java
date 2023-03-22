26cf38ab8bc30669696b42cf4fac41c7be7ffe40
Gulnara Che <g.chepurnova@gmail.com>
2023-03-22 12:01:39 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+    public class Main {
+        public static void main(String[] args) {
+
+            int price = 4530;
+            int priceBonusMile = 20;
+
+            System.out.println("Бонусные мили за покупку билета");
+            System.out.println((price/priceBonusMile) + " миль ");
+
+        }
+    }
