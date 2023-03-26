1795110f4c01eb87a752ab192770c7e3372bb33c
Gulnara Che <g.chepurnova@gmail.com>
2023-03-26 11:59:16 +0500

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 
-    public class Main {
-        public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
 
-            int price = 4530;
-            int priceBonusMile = 20;
+        int price = 4530;
+        int priceBonusMile = 20;
 
-            System.out.println("Бонусные мили за покупку билета");
-            System.out.println((price/priceBonusMile) + " миль ");
+        System.out.println("Бонусные мили за покупку билета");
+        System.out.println((price / priceBonusMile) + " миль ");
 
-        }
     }
+}
